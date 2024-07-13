83fddc1620f3174d038d0ac0b519b051bb89afe3
Farhan-Islam-Ifti <153084338+Farhan-Islam-Ifti@users.noreply.github.com>
2024-07-13 19:57:51 +0600

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import Login from './pages/Login';
 import axios from 'axios';
 import { Toaster } from 'react-hot-toast';
 
-axios.defaults.baseURL = 'http://localhost:8000';
+axios.defaults.baseURL = 'http://localhost:8000/';
 axios.defaults.withCredentials = true;
 
 function App() {
