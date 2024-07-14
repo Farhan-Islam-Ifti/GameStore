0ed0bd2e6e0c438dec1f11f237ebd6a04af5310e
Farhan-Islam-Ifti <153084338+Farhan-Islam-Ifti@users.noreply.github.com>
2024-07-14 12:18:27 +0600

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -7,7 +7,7 @@ const {test , registerUser, loginUser}= require('../controllers/authController')
 router.use(
     cors({
         credentials: true,
-        origin: 'http://localhost:5173'
+        origin: 'https://game-store-client.vercel.app'
     })
 )
 
